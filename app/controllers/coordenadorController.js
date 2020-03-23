79aea0fb7bd6ebef765a82c84e79863299369a9c
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-03-23 12:48:07 -0300

    Removendo métodos de controller e inserindo em model

--- app/controllers/coordenadorController.js
+++ app/controllers/coordenadorController.js
@@ -1,45 +1,21 @@
-const model = require("../models/coordenadorModel");
+const coordenadorModel = require("../models/coordenadorModel");
 const connection = require("../../config/dbConnection");
 
 exports.coordenador = (req, res) => {
-    connection.connect((err, client, release) => {
-        client.query("select * from coordenador", (err, result) => {
-            release();
-            let values = [];
-            for (var i = 0; i < result.rows.length; i++) {
-                values.push({
-                    id: result.rows[i].id,
-                    nome: result.rows[i].nome,
-                    sobrenome: result.rows[i].sobrenome,
-                    ativo: result.rows[i].ativo == false ? "Inativo" : "Ativo"
-                });
-            }
-            res.render("coordenador", { title: "Coordenador", data: values });
-        });
-    });
+    var coordenador = new coordenadorModel();
+    var text = "select * from coordenador";
+    coordenador.getAll(text, connection, res);
 };
 
 exports.getOne = (req, res) => {
-    query = {
-        text: "select * from coordenador where id = $1",
-        values: [req.params.id]
-    }
-    connection.connect((err, client, release) => {
-        client.query(query, (err, result) => {
-            release();
-            let values = {
-                id: result.rows[0].id,
-                nome: result.rows[0].nome,
-                sobrenome: result.rows[0].sobrenome,
-                ativo: result.rows[0].ativo
-            };
-            res.send(values);
-        });
-    });
+    var coordenador = new coordenadorModel();
+    var text = "select * from coordenador where id = $1";
+    var values = [req.params.id];
+    coordenador.getOne(text, values, connection, res);
 }
 
 exports.salvar = (req, res) => {
-    var coordenador = new model();
+    var coordenador = new coordenadorModel();
     if (parseInt(req.body.id) !== 0) {
         var text = "update coordenador set nome = $1, sobrenome = $2, ativo = $3 where id = $4";
         var values = [req.body.nome, req.body.sobrenome, req.body.ativo || false, req.body.id];
@@ -52,7 +28,7 @@ exports.salvar = (req, res) => {
 }
 
 exports.remover = (req, res) => {
-    var coordenador = new model();
+    var coordenador = new coordenadorModel();
     var text = "delete from coordenador where id = $1";
     var values = [req.params.id];
 
--- app/models/coordenadorModel.js
+++ app/models/coordenadorModel.js
@@ -12,4 +12,41 @@ Coordenador.prototype.executar = (text, values, conn) => {
     });
 }
 
+Coordenador.prototype.getOne = (text, values, conn, res) => {
+    query = {
+        text: text,
+        values: values
+    }
+    conn.connect((err, client, release) => {
+        client.query(query, (err, result) => {
+            release();
+            let values = {
+                id: result.rows[0].id,
+                nome: result.rows[0].nome,
+                sobrenome: result.rows[0].sobrenome,
+                ativo: result.rows[0].ativo
+            };
+            res.send(values);
+        });
+    });
+}
+
+Coordenador.prototype.getAll = (text, conn, res) => {
+    conn.connect((err, client, release) => {
+        client.query(text, (err, result) => {
+            release();
+            let values = [];
+            for (var i = 0; i < result.rows.length; i++) {
+                values.push({
+                    id: result.rows[i].id,
+                    nome: result.rows[i].nome,
+                    sobrenome: result.rows[i].sobrenome,
+                    ativo: result.rows[i].ativo == false ? "Inativo" : "Ativo"
+                });
+            }
+            res.render("coordenador", { title: "Coordenador", data: values });
+        });
+    });
+}
+
 module.exports = Coordenador;
