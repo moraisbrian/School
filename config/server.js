eec28f72df354488ab76f07bb0e25a92679919f4
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-03-25 11:42:31 -0300

    Alterações em rotas

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const handlebars = require("express-handlebars");
-const routes = require("../app/routes/routes");
 const app = express();
 
 app.engine('.hbs', handlebars({extname: '.hbs'}));
@@ -11,6 +10,17 @@ app.set("views", "./app/views");
 app.use(express.static("./app/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/", routes);
+
+const indexRouter = require("../app/routes/indexRoute");
+const alunoRouter = require("../app/routes/alunoRoute");
+const coordenadorRouter = require("../app/routes/coordenadorRoute");
+const cursoRouter = require("../app/routes/cursoRoute");
+const tipoCursoRouter = require("../app/routes/tipoCursoRoute");
+
+app.use("/", indexRouter);
+app.use("/aluno", alunoRouter);
+app.use("/coordenador", coordenadorRouter);
+app.use("/curso", cursoRouter);
+app.use("/tipocurso", tipoCursoRouter);
 
 module.exports = app;
